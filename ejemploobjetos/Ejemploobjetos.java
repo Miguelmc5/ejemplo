c59981301d8df8a6da924a73e189a4ae898bd2bd
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-11 08:40:57 +0200

    constructores

---
+++ ejemploobjetos/Ejemploobjetos.java
@@ -0,0 +1,24 @@
+package ejemploobjetos;
+
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class Ejemploobjetos {
+
+   
+    public static void main(String[] args) {
+        // instanciar un objeto
+    
+    Alumno persona=new Alumno();
+    
+    persona.darNota(7);
+    int valorNota=persona.devolverNota();
+    
+ System.out.println(valorNota);
+        
+ 
+ 
+    }
+    
+}
