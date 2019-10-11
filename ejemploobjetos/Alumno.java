aedacba6da88e1cbbac73db1dcb1ab9a2c1dca45
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-10-11 08:42:16 +0200

    segunda parte del constructor

---
+++ ejemploobjetos/Alumno.java
@@ -0,0 +1,33 @@
+package ejemploobjetos;
+
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class Alumno {
+    
+    private String nome;
+    private int nota;
+    
+    
+    public void darNota (int not){
+        
+        nota=not;     
+        
+    }    
+    public int devolverNota(){
+     
+        return nota;
+    
+    }
+    
+    public void mostrar(){
+        
+    System.out.println("");
+            
+           
+        
+    }
+       
+    
+}
